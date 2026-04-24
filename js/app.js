f76b2e0058423e1f0c901030b80804228e5f0787
craig matsambe <muchandura0@gmail.com>
2026-04-25 00:08:51 +0200

    Delete js directory

--- js/app.js
+++
@@ -1,4 +0,0 @@
-// Rotating icons logic (no speed control anymore)
-const rotatingIcons = document.querySelectorAll('.rotating-icon');
-
-// No longer need speed control buttons, so we just leave the rotation animation as is
